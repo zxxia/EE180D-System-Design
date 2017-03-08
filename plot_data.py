f4bbaa0deec6ef3620299ab17abe9ba5cab3c659
Zhengxu Xia <kevinxzx95@gmail.com>
2017-03-08 20:10:00 +0000

    Replaced the old py file

--- plot_data.py
+++ plot_data.py
@@ -19,47 +19,45 @@ def read_file(fname):
 	
 def main(): 
 	#signal_fname 	= "Acceleration_Walk_Dataset.csv"
-	#peaks_fname 	= "acceleration_output.csv"
-	#strides_fname = "acceleration_strides.csv"
-	signal_fname 	= "s3_30.csv"
-	peaks_fname 	= "peak.csv"
-	strides_fname = "stride.csv"
+	#extremes_fname 	= "acceleration_strides.csv"
+	signal_fname 	= "s5_17.csv"
+	extremes_fname 	= "s5_stride.csv"
 
 	# read data from file
 	signal 	= read_file(signal_fname)
-	peaks 	= read_file(peaks_fname)
-	strides = read_file(strides_fname)
+	extremes = read_file(extremes_fname)
 
 	# associate signals to names from signal matrix
-	t1 = signal[:, 0]
+	t1 = signal[:,0]
+	start_time = t1[0]
 	t2 = signal[:, 1]
-	t = [sum(x) for x in zip(t1, t2)]
-	t[:] = [x - t1[0] for x in t]
-	x = signal[:, 2]
+	t = [sum(x)/2.0 - start_time for x in zip(t1,t2)]
+	y = signal[:, 2]
+	x = signal[:, 3]
 
-	print(t)
 	#z = signal[:, 3]
 
 	# associate peaks and troughs to names from signal matrix
-	# P_i = peaks[:, 0]
-	# P_t = peaks[:, 1]
-	# P_y = peaks[:, 2]
-	# T_i = peaks[:, 3]
-	# T_t = peaks[:, 4]
-	# T_y = peaks[:, 5]
+	#S_i = extremes[:, 0]
+	S_t = extremes[:, 0]
+	S_x = extremes[:, 1]
+	#S_tt = extremes[:, 2]
+	#S_tx = extremes[:, 3]
+	
 
-	# # associate stridess from signal matrix
-	# S_i = strides[:, 0];
-	# S_t = strides[:, 1];
-	# S_x = strides[:, 2];
-
-	# fig, ax = plt.subplots()
-	# ax.plot(t, z, linewidth=0.8)
-	# ax.scatter(P_t, P_y, marker='^', color='g', s=50)
-	# ax.scatter(T_t, T_y, marker='h', color='r', s=50)
-	# #ax.scatter(S_t, S_x, marker='h', color='b', s=50)
-	# ax.scatter(S_t, S_x, marker='|', color='r', s=500000)
-	# plt.show()
+	fig, ax = plt.subplots()
+	fig.suptitle('Acceleration along x axis of acceleromter')
+	ax.plot(t, x, linewidth=0.8)
+	plt.xlabel('Time(s)')
+	#plt.xlim([0, 35])
+	plt.ylabel('Acceleration(m/s^2)')
+	#plt.ylim([-3.5, 4.5])
+	ax.scatter(S_t, S_x, marker='o', color='g', s=50)
+	#ax.scatter(S_tt, S_tx, marker='o', color='r', s=50)
+	# for i in range(0, len(S_i)):
+	# 	if(S_x[i] > 0):
+	# 		ax.axvline(S_t[i], color='r', linestyle='--')
+	plt.show()
 	
 if __name__ == "__main__":
 	main()
